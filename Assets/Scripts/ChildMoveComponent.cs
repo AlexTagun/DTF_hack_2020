bdb5dda4a2e6d36bd2d7e316c91d464e4af8aaa4
alexeytagun <alexey.tagun@game-garden.com>
2020-07-18 17:09:01 +0300

    add stairs logic

---
+++ Assets/Scripts/ChildMoveComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildMoveComponent : MonoBehaviour {
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    private void Update() {
+        var curPos = new Vector2(transform.position.x, transform.position.y);
+        curPos.x += speed + Time.deltaTime;
+        _rigidbody.MovePosition(curPos);
+    }
+}
---
+++ Assets/Scripts/Stairs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stairs : MonoBehaviour {
+    [SerializeField] private Transform _endPoint;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Child") {
+            StartCoroutine(DoTransportAnimation(other.gameObject));
+        }
+    }
+
+    private IEnumerator DoTransportAnimation(GameObject kid) {
+        kid.transform.position = _endPoint.position;
+        yield return null;
+    }
+
+}
