0d707419718da861fbdd1cc4a4f2b891157aeeb8
AlexKutsar <alekcandr_123qw@mail.ru>
2020-07-18 16:03:38 +0300

    add drag and drop child

---
+++ Assets/Scripts/DragDropChild.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropChild : MonoBehaviour
+{
+    [SerializeField] private HingeJoint2D _hingeJoint;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    private Camera _camera = null;
+    private bool _isMouseDown = false;
+    private Vector2 CurPosition => new Vector2(transform.position.x, transform.position.y);
+    private Vector2 MousePosition => GetMousePosition();
+    private Vector2 _mouseDelta;
+
+    void Start()
+    {
+        _camera = Camera.main;
+        _hingeJoint.enabled = false;
+    }
+    private Vector2 GetMousePosition()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        mousePos = _camera.ScreenToWorldPoint(mousePos);
+        return new Vector2(mousePos.x, mousePos.y);
+    }
+
+
+    private void OnMouseDown()
+    {
+        _isMouseDown = true;
+        _hingeJoint.enabled = true;
+        _mouseDelta = CurPosition - MousePosition;
+    }
+
+    private void OnMouseUp()
+    {
+        _isMouseDown = false;
+        _hingeJoint.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_isMouseDown) return;
+        //_rigidbody.MovePosition(MousePosition + _mouseDelta);
+    }
+
+}
---
+++ Assets/Scripts/FollowCursor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursor : MonoBehaviour
+{
+    private Camera _camera;
+    private Rigidbody2D _rigidbody2D;
+
+    [SerializeField] private float moveSpeed;
+
+    private Vector3 mousePosition;
+    private Vector2 position;
+    void Start()
+    {
+        _camera = Camera.main;
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        transform.position = _camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.MovePosition(position);
+    }
+}
