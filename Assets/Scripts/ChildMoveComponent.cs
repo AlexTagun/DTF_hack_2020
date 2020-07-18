09eff6000a353d1be1d493fc2015815b8a1c51fb
alexeytagun <alexey.tagun@game-garden.com>
2020-07-18 19:03:49 +0300

    add main scene + sprites

--- Assets/Scripts/ChildMoveComponent.cs
+++ Assets/Scripts/ChildMoveComponent.cs
@@ -6,9 +6,36 @@ using UnityEngine;
 public class ChildMoveComponent : MonoBehaviour {
     [SerializeField] private float speed;
     [SerializeField] private Rigidbody2D _rigidbody;
+
+    private MoveState _state;
+
+    private void Awake() {
+        MoveRight();
+    }
+
+    private enum MoveState {
+        Right,
+        Left,
+        Idle
+    }
+
+    public void Stop() {
+        _state = MoveState.Idle;
+    }
+
+    public void MoveLeft() {
+        _state = MoveState.Left;
+        
+    }
+
+    public void MoveRight() {
+        _state = MoveState.Right;
+    }
     private void Update() {
+        if(_state == MoveState.Idle) return;
+        
         var curPos = new Vector2(transform.position.x, transform.position.y);
-        curPos.x += speed + Time.deltaTime;
+        curPos.x += speed * Time.deltaTime * (_state == MoveState.Left ? -1 : 1);
         _rigidbody.MovePosition(curPos);
     }
 }
