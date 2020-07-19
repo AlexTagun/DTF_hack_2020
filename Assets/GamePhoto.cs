639dd025bee8eade4f58af6b2d6125a88bff8da3
alexeytagun <alexey.tagun@game-garden.com>
2020-07-20 02:30:00 +0300

    add flash effect

--- Assets/GamePhoto.cs
+++ Assets/GamePhoto.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GamePhoto : MonoBehaviour
 {
@@ -9,7 +11,10 @@ public class GamePhoto : MonoBehaviour
 
     public void doPhoto() {
         int thePhotoScores = getScoresByPhoto();
-
+        _flash.DOFade(1, 0);
+        _flash.DOFade(0, 0.5f);
+        _audioSource.time = 0.8f;
+        _audioSource.Play();
         if (thePhotoScores >= _bestPhotoScore) {
             StartCoroutine(capture.doCapture(_camera, (Texture2D inTexture)=>{
                 _bestPhotoTexture = inTexture;
@@ -55,6 +60,8 @@ public class GamePhoto : MonoBehaviour
 
     //Fields
     [SerializeField] private Camera _camera = null;
+    [SerializeField] private SpriteRenderer _flash = null;
+    [SerializeField] private AudioSource _audioSource = null;
     [SerializeField] private KeyCode _activationKey = KeyCode.Space;
 
     private ChildAI[] _children = null;
